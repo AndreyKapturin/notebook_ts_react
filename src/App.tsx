2a8e2244e21d7dec0909f1409be2a24faf006644
Andrey Kapturin <andruxa21097@gmail.com>
2024-09-03 11:35:32 +0700

    feat: Отображение списка заметок. Просмотр и редактирование содержимого заметки

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { nanoid } from 'nanoid';
+
+interface INote {
+  id: string;
+  body: string;
+}
 
 function App() {
+  const [ currentNoteIndex, setCurrentNoteIndex ] = useState<number | null>(null);
+  const initial: INote[] = [
+    {
+      id: nanoid(),
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt ratione corrupti quasi nihil similique. Accusamus veritatis rem placeat! Laborum est praesentium fuga tempore deserunt voluptatem libero soluta non accusamus.',
+    },
+    {
+      id: nanoid(),
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt ratione corrupti quasi nihil similique. Accusamus veritatis rem placeat! Laborum est praesentium fuga tempore deserunt voluptatem libero soluta non accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt ratione corrupti quasi nihil similique. Accusamus veritatis rem placeat! Laborum est praesentium fuga tempore deserunt voluptatem libero soluta non accusamus.',
+    },
+    {
+      id: nanoid(),
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt ratione corrupti quasi nihil similique. Accusamus veritatis rem placeat! Laborum est ',
+    },
+  ]
+  const [ notes, setNotes ] = useState<INote[]>(initial);
+
+  function editNote(newBody: string): void {
+    setNotes(prev => {
+      return prev.map((note, i) => {
+        return i === currentNoteIndex ? {...note, body: newBody} : note
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <section className='notebook'>
+        <h1>Блокнот</h1>
+        <aside className='notebook__sidebar'>
+          <button className='notebook__button'>Добавить</button>
+          <ol className='notebook__notelist'>
+            {notes.map((note, i) => (
+              <li
+                className='notelist__item'
+                key={note.id}
+                onClick={() => setCurrentNoteIndex(i)}
+              >
+                {note.body.slice(0, 25) + '...'}
+              </li>
+            ))}
+          </ol>
+        </aside>
+        <main className='notebook__main'>
+          <textarea
+            className='notebook__textarea'
+            value={currentNoteIndex !== null ? notes[currentNoteIndex].body : ''}
+            onChange={(e) => editNote(e.target.value)}
+            disabled={currentNoteIndex === null}
+          />
+        </main>
+      </section>
     </div>
   );
 }
